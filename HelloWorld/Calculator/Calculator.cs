831d1f0f8e268913bb9e1d045e21473def3d12f4
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-03-20 16:13:46 -0700

    Created a calculator in Math namespace

---
+++ HelloWorld/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Math
+{
+    public class Calculator
+    {
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        public static float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Divide(float a, float b)
+        {
+            return (float)a / (float)b;
+        }
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Math;
 
-namespace HelloWorld
+namespace Program
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var a = Math.Calculator.Add(1, 4);
+            var b = Math.Calculator.Subtract(401, 404);
+            var c = Math.Calculator.Divide(40, 3);
+            var d = Math.Calculator.Multiply(10, 405);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
         }
     }
 }
