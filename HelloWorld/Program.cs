486cbf6fcc4a9e30e910aeb1bc49bb87158e4186
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-03-22 13:28:00 -0700

    String.Join and formatted strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HelloWorld.Math;
+using HelloWorld.Strings;
 
 namespace Program
 {
@@ -8,16 +9,23 @@ namespace Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            CalculateThings();
+
+            var ints = new int[5] { 5, 10, 15, 20, 25 };
+            for (int i = 0; i < ints.Length; i++)
+            {
+                CalculateThings(ints[i]);
+            }
             LogicThings();
+            MadLib();
+            ListStringsOut();
         }
 
-        public static void CalculateThings()
+        public static void CalculateThings(int i)
         {
-            var a = Calculator.Add(1, 4);
-            var b = Calculator.Subtract(401, 404);
-            var c = Calculator.Divide(40, 3);
-            var d = Calculator.Multiply(10, 405);
+            var a = Calculator.Add(i, 4);
+            var b = Calculator.Subtract(i, 404);
+            var c = Calculator.Divide(i, 3);
+            var d = Calculator.Multiply(i, 405);
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
@@ -27,6 +35,19 @@ namespace Program
         public static void LogicThings()
         {
             bool a = LogicalCalculator.And(true, false);
+            Console.WriteLine(a);
+        }
+
+        public static void MadLib()
+        {
+            string a = UseYourWords.FormatMadLibString("red", "rabbit");
+            Console.WriteLine(a);
+        }
+
+        public static void ListStringsOut()
+        {
+            var arr = new string[5] { "John", "Jacob", "Jingle", "Himer", "Smith" };
+            string a = UseYourWords.ListThePeople(arr, 5);
             Console.WriteLine(a);
         }
     }
---
+++ HelloWorld/Strings/UseYourWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld.Strings
+{
+    public class UseYourWords
+    {
+        public static string FormatMadLibString(string a, string b)
+        {
+            return String.Format("The {0} came close to {1} the end.", a, b);
+        }
+
+        public static string ListThePeople(string[] a, int length)
+        {
+            string concatString = "With for loop: ";
+            for (int i = 0; i < length; i++)
+            {
+                concatString += a[i];
+                if (i != length-1)
+                {
+                    concatString += ", ";
+                }
+            }
+            var b = String.Join(", ", a);
+            concatString += "\nWith String.Join: ";
+            concatString += b;
+            return concatString;
+        }
+    }
+}
